250a4b704aecb3a443475d63371c49d855cce191
catatsuy <catatsuy@catatsuy.org>
2023-07-16 16:24:49 +0900

    Update LibreSSL version to 3.5.3

--- builder/const.go
+++ builder/const.go
@@ -20,7 +20,7 @@ const (
 
 // libressl
 const (
-	// datasource=github-tags depName=libressl/portable
+	// datasource=github-tags depName=libressl/portable versioning=v?(?<major>\\d+)\\.(?<minor>\\d+)\\.(?<patch>\\d+)?
 	LibreSSLVersion           = "3.5.3"
 	LibreSSLDownloadURLPrefix = "https://ftp.openbsd.org/pub/OpenBSD/LibreSSL"
 )
